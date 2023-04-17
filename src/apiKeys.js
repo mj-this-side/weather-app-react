99c55c704fa82bfc112d51227ca5541e108c5a7c
Mrityunjay Dixit <dixitmrityunjay321@gmail.com>
2023-04-17 17:05:30 +0530

    updated

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,7 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
+
 module.exports = {
     key: "44004b7474f457dab21306b7f422b9bc",
     base: "https://api.openweathermap.org/data/2.5/",
   };
+
+  // api data
