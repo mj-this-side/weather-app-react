cad2851e0b05bc8291abfb2be11d1f6a26e35f03
Mrityunjay Dixit <dixitmrityunjay321@gmail.com>
2023-04-17 16:38:48 +0530

    weather app created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import CurrentLocation from "./currentLocation";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "44004b7474f457dab21306b7f422b9bc",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
